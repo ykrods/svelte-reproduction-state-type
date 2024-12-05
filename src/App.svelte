524f1f0173ec0e44a863e29f179982c6830682f0
ykrods <890082+ykrods@users.noreply.github.com>
2024-12-05 19:35:06 +0900

    Add fix proposal

--- src/App.svelte
+++ src/App.svelte
@@ -3,6 +3,12 @@
 
   let p: Person | undefined = $state(undefined)
 
+  /**
+   * let p = $state(undefined as (Person | undefined));
+   * let p = $state<Person | undefined>(undefined);
+   * let p: Person | undefined = $state()
+   */
+
   $effect(() => {
     // load person
     setTimeout(() => {
@@ -10,6 +16,11 @@
     }, 1000);
   });
 
+  $effect(() => {
+    console.log(p?.name);
+  });
+
   let displayName = $derived(p?.name ?? "anon")
+  // let displayName = $derived.by(() => p?.name ?? "anon")
 </script>
 <div>{ displayName }</div>
